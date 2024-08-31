b655f6f7841f8fde807c0aaddc4b3804638e60ee
Ishihara <almond1024r@gmail.com>
2024-08-31 16:16:36 +0900

    hide APIkey

--- api.rb
+++
@@ -1,8 +0,0 @@
-require 'open-uri'
-require 'json'
-require 'date'
-
-url = "https://newsapi.org/v2/everything?q=sol&sortBy=popularity&apiKey=98ff65e5958a47d4a3139a200e9df349"
-response = URI.open(url).read
-parsed_response = JSON.parse(response)
-puts parsed_response
--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -11,7 +11,6 @@ class CoinsController < ApplicationController
   # 工夫点　１次情報のみ活用した点 Gem作成者のGiHubを見て書いた　２次情報を見ていない
 
   def news
-    
     url = "#{NEWS_API_BASE_URL}?q=bitcoin&sortBy=poularity&apiKey=#{NEWS_API_KEY}"
     response = self.class.get(url)
     render json:response.parsed_response
